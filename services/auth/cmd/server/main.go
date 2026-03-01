c3a3f86007df2e2aaf955ec894d52b4f71fb226c
n.vawntien <ngvantien1526@gmail.com>
2026-03-01 18:22:59 +0700

    feat: Enhance local development with Mailhog, expose database ports, and refactor auth service logging and Docker configuration.

--- services/auth/cmd/server/main.go
+++ services/auth/cmd/server/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	cfg, err := config.LoadConfig("../../config")
+	cfg, err := config.LoadConfig("/app/config")
 	if err != nil {
 		log.Fatalf("load config failed: %v", err)
 	}
